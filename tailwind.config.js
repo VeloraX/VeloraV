7d9f84c07a07741f97c3ad32100102ec75a51926
codemikko <hellomikko@outlook.com>
2024-07-11 22:07:55 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  content: [
+    "./_site/**/*.html",
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./*.html"
+  ],
+  // Add in tailwind.config.js
+  theme: {
+    extend: {
+      colors: {
+        nord: {
+          // Define your Nord color palette here
+          // Example:
+          "0": "#2E3440",
+          "1": "#3B4252",
+          "2": "#434C5E",
+          "3": "#4C566A",
+          "4": "#D8DEE9",
+          "5": "#E5E9F0",
+          "6": "#ECEFF4",
+          "7": "#8FBCBB",
+          "8": "#88C0D0",
+          "9": "#81A1C1",
+          "10": "#5E81AC",
+          "11": "#BF616A",
+          "12": "#D08770",
+          "13": "#EBCB8B",
+          "14": "#A3BE8C",
+          "15": "#B48EAD"
+        }
+      },
+    },
+  },
+
+  plugins: [
+    require('daisyui'),
+  ],
+}
